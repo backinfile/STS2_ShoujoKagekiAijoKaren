0639ecdfa64a5972a8278abf6d79a6375974dd52
backinfile <1757518497@qq.com>
2026-03-18 03:41:24 +0800

    update

---
+++ src/Core/SKCardTrail.cs
@@ -0,0 +1,9 @@
+using Godot;
+using MegaCrit.Sts2.Core.Nodes.Vfx;
+
+namespace WatcherMod.Core;
+
+[GlobalClass]
+public partial class SKCardTrail : NCardTrail
+{
+}
---
+++ src/Core/SKMerchantCharacter.cs
@@ -0,0 +1,9 @@
+using Godot;
+using MegaCrit.Sts2.Core.Nodes.Screens.Shops;
+
+namespace WatcherMod.Core;
+
+[GlobalClass]
+public partial class SKMerchantCharacter : NMerchantCharacter
+{
+}
