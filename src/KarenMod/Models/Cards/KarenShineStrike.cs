da9de4827f04d8967c24973e1d425c7be278fe45
backinfile <1757518497@qq.com>
2026-03-18 23:11:11 +0800

    update

--- src/KarenMod/Models/Cards/KarenShineStrike.cs
+++ src/KarenMod/Models/Cards/KarenShineStrike.cs
@@ -1,12 +1,15 @@
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 using ShoujoKagekiAijoKaren.src.KarenMod.DynamicVars;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ShoujoKagekiAijoKaren.src.Models.Cards;
@@ -28,6 +31,18 @@ public sealed class KarenShineStrike : CardModel
         new DamageVar(9m, ValueProp.Move),
         new KarenShineVar(3m)
     };
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips
+    {
+        get
+        {
+            var tips = base.ExtraHoverTips.ToList();
+            var shineTitle = new LocString("cards", "KAREN_SHINE_KEYWORD.title");
+            var shineDesc = new LocString("cards", "KAREN_SHINE_KEYWORD.description");
+            tips.Add(new HoverTip(shineTitle, shineDesc));
+            return tips;
+        }
+    }
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
