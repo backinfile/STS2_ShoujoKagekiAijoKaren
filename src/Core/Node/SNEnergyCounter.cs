b6e2249b91fe67ac5e334009a5c00b281900e9c6
backinfile <1757518497@qq.com>
2026-04-20 02:00:14 +0800

    update

--- src/Core/Node/SNEnergyCounter.cs
+++ src/Core/Node/SNEnergyCounter.cs
@@ -1,8 +1,21 @@
 using Godot;
+using HarmonyLib;
 using MegaCrit.Sts2.Core.Nodes.Combat;
 
 namespace ShoujoKagekiAijoKaren.Core;
 
 public partial class SNEnergyCounter : NEnergyCounter
 {
+    public SNParticlesContainer _myBackVfx;
+    public SNParticlesContainer _myFrontVfx;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        AccessTools.Field(typeof(NEnergyCounter), "_backVfx")?.SetValue(this, null);
+        AccessTools.Field(typeof(NEnergyCounter), "_frontVfx")?.SetValue(this, null);
+
+        _myBackVfx = GetNode<SNParticlesContainer>("%MyEnergyVfxBack");
+        _myFrontVfx = GetNode<SNParticlesContainer>("%MyEnergyVfxFront");
+    }
 }
--- src/Core/Patches/NEnergyCounterOnEnergyChangedPatch.cs
+++ src/Core/Patches/NEnergyCounterOnEnergyChangedPatch.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using HarmonyLib;
 using MegaCrit.Sts2.Core.Nodes.Combat;
 using ShoujoKagekiAijoKaren.Core;
@@ -8,13 +9,21 @@ namespace ShoujoKagekiAijoKaren.src.Core.Patches;
 /// 在 NEnergyCounter.OnEnergyChanged 中，当能量增加时，
 /// 如果实例是 SKEnergyCounter，也触发其子类自定义的 _myBackVfx / _myFrontVfx。
 /// </summary>
-[HarmonyPatch(typeof(NEnergyCounter), "OnEnergyChanged")]
+[HarmonyPatch]
 public class NEnergyCounterOnEnergyChangedPatch
 {
-    private static void Postfix(NEnergyCounter __instance, int oldEnergy, int newEnergy)
+    static MethodBase TargetMethod()
     {
+        var method = AccessTools.Method(typeof(NEnergyCounter), "OnEnergyChanged", new[] { typeof(int), typeof(int) });
+        MainFile.Logger.Info($"[NEnergyCounterOnEnergyChangedPatch] TargetMethod resolved: {method?.FullDescription() ?? "NULL"}");
+        return method;
+    }
+
+    public static void Postfix(NEnergyCounter __instance, int oldEnergy, int newEnergy)
+    {
+        MainFile.Logger.Info($"[NEnergyCounterOnEnergyChangedPatch] Postfix called, old={oldEnergy}, new={newEnergy}, instanceType={__instance.GetType().Name}");
         if (oldEnergy >= newEnergy) return;
-        if (__instance is SKEnergyCounter counter)
+        if (__instance is SNEnergyCounter counter)
         {
             MainFile.Logger.Info("========== OnEnergyChanged");
             counter._myBackVfx?.Restart();
